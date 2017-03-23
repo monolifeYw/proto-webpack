f428e45204d27179d77c7fde4918c681820018bc
monolifeYw <lyw31136@naver.com>
2017-03-23 16:38:58 +0900

    [server] server.config.js first

--- mywork/readme.js
+++ mywork/readme.js
@@ -50,3 +50,56 @@ if (!window._babelPolyfill) {
 npm install webpack --save-dev
 npm install babel-polyfill --save-dev
 npm install babel-loader --save-dev
+
+"babel-core": "^6.24.0",
+"babel-loader": "^6.4.1",
+"babel-plugin-transform-class-properties": "^6.23.0",
+"babel-plugin-transform-object-rest-spread": "^6.23.0",
+"babel-polyfill": "^6.23.0",
+"babel-preset-latest": "^6.24.0",
+"css-loader": "^0.27.3",
+"eslint": "^3.18.0",
+"eslint-loader": "^1.6.3",
+"extract-text-webpack-plugin": "^2.1.0",
+"node-sass": "^4.5.1",
+"sass-loader": "^6.0.3",
+"style-loader": "^0.15.0",
+"webpack": "^2.2.1",
+"webpack-dev-server": "^2.4.2"
+
+
+======  Server Dependencies =======
+npm install express --save // server framework
+npm install morgan --save  // logger
+npm install rimraf --save  // folder/file remove
+npm install express-handlebars --save // template
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ mywork/server.config.js
@@ -0,0 +1,45 @@
+// import 
+var express = require('express');
+var logger = require('morgan');
+var exphbs = require('express-handlebars');
+
+var router = require('./routes/common-routes');
+
+var app = express();
+
+// set environment //
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+// set middleware //
+// public path
+app.use(express.static(__dirname, './public'));
+
+// set view engine
+app.set('view engine', 'hbs');
+app.set('views', __dirname + 'views');
+
+// set logger 
+app.use(logger('short'));
+
+// set router
+app.use(router);
+
+// set error
+// 404
+app.use(function (req, res, next) {
+  var err = new Error('not found');
+  err.status = 404;
+  next(err);
+});
+
+// Base error
+app.use(function (err, req, res, next) {
+  var status = err.status || 500;
+  res.status(status).render('error', {status: status, layout: false});
+});
+
+
+// set server 
+app.listen(8080, function () {
+  console.log('Server Start!!!');
+});
