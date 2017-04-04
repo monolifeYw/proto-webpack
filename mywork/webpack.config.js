a9581abbb450d1809da1452f79a10826352b8215
monolifeYw <lyw31136@naver.com>
2017-04-04 14:17:50 +0900

    [webpack]finish in this repo

--- mywork/webpack.config.js
+++ mywork/webpack.config.js
@@ -9,6 +9,20 @@ if (ENV.SVR_HMR_MODE) {
     webpackConfig.entry[prop].unshift('webpack/hot/dev-server');
   });
 
+  /*
+  https://github.com/glenjamin/webpack-hot-middleware
+  
+  path - The path which the middleware is serving the event stream on
+  name - Bundle name, specifically for multi-compiler mode
+  timeout - The time to wait after a disconnection before attempting to reconnect
+  overlay - Set to false to disable the DOM-based client-side overlay.
+  reload - Set to true to auto-reload the page when webpack gets stuck.
+  noInfo - Set to true to disable informational console logging.
+  quiet - Set to true to disable all console logging.
+  dynamicPublicPath - Set to true to use webpack publicPath as prefix of path. 
+  (We can set __webpack_public_path__ dynamically at runtime in the entry point, see note of output.publicPath)
+   */
+
   webpackConfig.entry['wds'] = 'webpack-hot-middleware/client?path=/__webpack_hmr';
   // webpackConfig.entry['wds'] = 'webpack-hot-middleware/client?' + ENV.SVR_WDS_PATH;
 }
